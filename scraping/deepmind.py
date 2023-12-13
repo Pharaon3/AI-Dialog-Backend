c83950eaa29a99111767e91fc7d15768259cf6e2
Pharaon3 <Pharaon313@outlook.com>
2023-12-13 16:06:10 +0900

    deepmind modified

--- scraping/deepmind.py
+++ scraping/deepmind.py
@@ -26,18 +26,18 @@ options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://deepmind.google/")
-time.sleep(3)
-
 current_datetime = datetime.now()
 formatted_datetime = current_datetime.strftime("%Y/%m/%d %H:%M:%S")
 
 outData = []
+
+driver.get("https://deepmind.google/discover/blog/")
+time.sleep(1)
 elements = driver.find_elements(By.CSS_SELECTOR, "a.glue-card")
 for c in range(0, len(elements)):
     try:
         link = elements[c].get_attribute('href')
-        source = elements[c].find_element(By.TAG_NAME, "source").get_attribute('srcset')
+        source = elements[c].find_elements(By.TAG_NAME, "img")[0].get_attribute('src')
         title = elements[c].find_element(By.CSS_SELECTOR, "p.glue-headline").get_attribute('innerHTML')
         content = elements[c].find_element(By.CSS_SELECTOR, "p.glue-card__description").get_attribute('innerHTML')
         print("link: " + link)
